8a1a3e2b5ae6bc4921b141a3b7f4d6adb222f7bf
niteshzeft <niteshsingh13572@gmail.com>
2019-10-20 10:57:19 +0545

    tab icon added

--- App.js
+++ App.js
@@ -12,17 +12,60 @@ import {Platform,
   Text, 
   View} from 'react-native';
 
+import {createBottomTabNavigator} from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
 import FirstScreen from './FirstScreen'
 import SecondScreen from './SecondScreen'
 import ThirdScreen from './ThirdScreen'
 
+let Tab = createBottomTabNavigator({
+  First:{
+    screen: FirstScreen,
+    navigationOptions: {
+      tabBarLabel: "First",
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-home" color={tintColor} size={24} />
+        )
+    }
+  },
+
+  Second:{
+    screen: SecondScreen,
+    navigationOptions: {
+      tabBarLabel: "Second",
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="md-person" color={tintColor} size={24} />
+        )
+    }
+  },
+
+  Third:{
+    screen: ThirdScreen,
+    navigationOptions: {
+      tabBarLabel: "Third",
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-call" color={tintColor} size={24} />
+        )
+    }
+  }
+
+},{
+  tabBarOptions: {
+    activeTintColor: "#283593",
+    inactiveTintColor: "grey",
+    labelStyle: {
+      fontSize: 16,
+      fontWeight: "bold"
+    }
+  }
+})
 
 export default class App extends Component {
   render() {
     return (
-      //<FirstScreen/>
-      //<SecondScreen/>
-      <ThirdScreen/>
+      <Tab/>
       
     );
   }
